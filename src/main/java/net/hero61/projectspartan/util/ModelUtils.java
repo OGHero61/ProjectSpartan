5cee0cba2834277d56dd270e970609a1fb4ce20e
OGHero61 <joshjackson200361@gmail.com>
2024-07-29 22:07:03 -0400

    Item Textures

--- src/main/java/net/hero61/projectspartan/util/ModelUtils.java
+++ src/main/java/net/hero61/projectspartan/util/ModelUtils.java
@@ -7,9 +7,7 @@ import net.minecraft.client.model.geom.builders.PartDefinition;
 
 public class ModelUtils {
 
-    /**
-     * Solve some model tomfuckery and stop mc bitching
-     */
+
     public static MeshDefinition createBlankHumanoidMesh() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
